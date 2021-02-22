612c9150250fdb57487af7dbd1cf30525fe4a950
dddepg <924144294@qq.com>
2021-02-22 13:17:02 +0800

    重新打包

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,29 @@ module.exports = {
   publicPath: "/graduate_design",
   assetsDir: "static",
   productionSourceMap: false,
+  pluginOptions: {
+    compression: {
+      brotli: {
+        filename: "[path].br[query]",
+        algorithm: "brotliCompress",
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        compressionOptions: {
+          level: 11,
+        },
+        minRatio: 0.8,
+      },
+      gzip: {
+        filename: "[path].gz[query]",
+        algorithm: "gzip",
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        minRatio: 0.8,
+      },
+      configureWebpack: {
+        // 关闭 webpack 的性能提示
+         performance: {
+           hints:false
+         }
+      },    
+    },
+  },
 };
